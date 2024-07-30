152a227915103c0cb5a676fc2f62a3df416d3173
mkatile <mariamkatile06@gmail.com>
2024-07-29 20:04:10 -0400

    update schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,25 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-
 CREATE TABLE departments (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department (id)
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role (id),
-    FOREIGN KEY (manager_id) REFERENCES employee (id)
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
